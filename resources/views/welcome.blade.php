c9ef0ad375d3f6b8be0e1db2c62c590431567951
Nicolas Figueira <nico.figueira.mahe@gmail.com>
2018-09-24 16:35:42 +0200

    Update dependencies

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,16 +87,6 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
-                <form action="/postfiles" enctype="multipart/form-data" method="POST">
-                    <p>
-                        <label for="photo">
-                            <input type="file" name="photo" id="photo">
-                        </label>
-                    </p>
-                    <button>Upload</button>
-                    {{ csrf_field() }}
-                </form>
             </div>
         </div>
     </body>
